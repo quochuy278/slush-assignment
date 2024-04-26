3df726d3a71554f68f0c6a4d5947c75b624eeb0b
quochuy278 <quochuy270800@gmail.com>
2024-04-26 22:23:26 +0300

    use cors and body parser in app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,19 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
+
+import todoRoutes from "./routes/todo";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(todoRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
