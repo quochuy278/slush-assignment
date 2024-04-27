3b27e56b15810eebb9bd484a85a780ed2bdfd19a
quochuy278 <quochuy270800@gmail.com>
2024-04-27 19:53:29 +0300

    server: execute checking migration and connect to databae when starting the server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,10 @@
-import express, { Express, Request, Response } from "express";
-import cors from "cors";
 import bodyParser from "body-parser";
+import cors from "cors";
 import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
 
-import todoRoutes from "./routes/todo";
+import { combinedRoutes } from "./routes";
+import { checkAndMigrate, connectToDatabase } from "./services/database";
 
 dotenv.config();
 
@@ -13,11 +14,17 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(todoRoutes);
+app.use(combinedRoutes);
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+  res.send("Welcome to slush assignment api server");
 });
+
+// Tracking database migration
+checkAndMigrate();
+
+// Initialize connection to database
+connectToDatabase();
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
