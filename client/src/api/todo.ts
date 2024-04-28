39353b4927a746e300a6d3713d9738a0a4e54681
quochuy278 <quochuy270800@gmail.com>
2024-04-28 21:12:26 +0300

    client: add api for auth and todo operation

---
+++ client/src/api/todo.ts
@@ -0,0 +1,69 @@
+import axios from "axios";
+import useAuth from "../store/useAuth";
+import { Todo } from "../features/Todo/Todo";
+
+const baseURL = import.meta.env.VITE_APP_API_URL;
+
+const accessToken = useAuth.getState().user?.accessToken;
+
+const fetchTodos = async () => {
+  try {
+    const result = await axios.get(`${baseURL}/todos`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    return result.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const createTodo = async (data: {
+  name: string;
+  description: string;
+  ready: boolean;
+}) => {
+  try {
+    const result = await axios.post(`${baseURL}/todo`, data, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    return result.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const updateTodo = async (data: Todo) => {
+  try {
+    const result = await axios.patch(`${baseURL}/todo/${data.id}`, data, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    return result.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteTodo = async (data: Todo) => {
+  try {
+    const result = await axios.delete(`${baseURL}/todo/${data.id}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    return result.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { fetchTodos, createTodo, updateTodo, deleteTodo };
