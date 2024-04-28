5c69d8777a0d97f7cfab675ed704c46f1cf2e6a4
quochuy278 <quochuy270800@gmail.com>
2024-04-28 21:17:16 +0300

    server: add dto for user and todo

--- server/src/services/repository/todo/dto/todo.dto.ts
+++ server/src/services/repository/todo/dto/todo.dto.ts
@@ -1,7 +1,25 @@
+import { IsBoolean, IsNotEmpty, IsString } from "class-validator";
+
 export type TodoDto = {
+  id: number;
+  public_id: string;
   name: string;
   description: string;
   created_at: string;
   ready: boolean;
-  userId: number;
+  user_id: number;
 };
+
+export class ValidatedTodo {
+  @IsString()
+  @IsNotEmpty()
+  name!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description!: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  ready!: boolean;
+}
--- server/src/services/repository/user/dto/user.dto.ts
+++ server/src/services/repository/user/dto/user.dto.ts
@@ -1,6 +1,25 @@
+import { IsEmail, IsNotEmpty, IsString, isEmail } from "class-validator";
+
 export type UserDto = {
   id: number;
+  public_id: string;
   name: string;
   email: string;
-  password: string;
+  hash: string;
 };
+
+export class ValidatedUser {
+  @IsString()
+  @IsNotEmpty()
+  email!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password!: string;
+}
+
+export class ValidateSignUpUser extends ValidatedUser {
+  @IsString()
+  @IsNotEmpty() // Optional if you want to enforce a non-empty email
+  name!: string;
+}
