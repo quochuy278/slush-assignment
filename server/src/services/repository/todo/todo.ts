cdca5fd08c55a9d29979da90e0bf5d7720d0a943
quochuy278 <quochuy270800@gmail.com>
2024-04-28 21:17:39 +0300

    server: add database service for todo and user

--- server/src/services/repository/todo/todo.ts
+++ server/src/services/repository/todo/todo.ts
@@ -1,16 +1,21 @@
-import { client } from "../../database";
+import { Client } from "pg";
+import { dbConfig } from "../../database";
 import { TodoDto } from "./dto/todo.dto";
+import logger from "../../logger";
 
-async function addTodo(todo: TodoDto, userId: number): Promise<TodoDto> {
-  const { name, description, ready } = todo; // Destructure todo object
+async function addTodo(
+  todo: Omit<TodoDto, "id" | "created_at" | "user_id">,
+  userId: number
+): Promise<TodoDto> {
+  const client = new Client(dbConfig);
+  const { name, description, ready, public_id } = todo; // Destructure todo object
 
   try {
     await client.connect();
 
-    // Assuming you have already executed the migration script to create the 'todos' table
     const query = `
-      INSERT INTO todos (name, description, ready, user_id)
-      VALUES ($1, $2, $3, $4)
+      INSERT INTO todos (name, description, ready, user_id, public_id)
+      VALUES ($1, $2, $3, $4, $5)
       RETURNING *; -- Return the inserted todo record
     `;
 
@@ -19,26 +24,53 @@ async function addTodo(todo: TodoDto, userId: number): Promise<TodoDto> {
       description,
       ready,
       userId,
+      public_id,
     ]);
-    const newTodo = result.rows[0];
 
-    console.log(`Todo added successfully:`, newTodo);
+    const newTodo = result.rows[0];
 
     return newTodo; // Return the inserted todo object
   } catch (error) {
-    console.error("Error adding todo:", error);
+    logger.error("Error adding todo:", error);
     throw error; // Re-throw the error for further handling
   } finally {
     await client.end();
   }
 }
 
+async function getTodo(todoPublicId: string) {
+  const client = new Client(dbConfig);
+
+  try {
+    const query: string = `
+    SELECT *
+    FROM todos
+    WHERE public_id = $1
+  `;
+    const result = await client.query(query, [todoPublicId]);
+
+    // Check if any user was found
+    if (result.rows.length === 0) {
+      return null; // Return null if no user found
+    }
+
+    // Return the first user from the results (assuming unique emails)
+    return result.rows[0];
+  } catch (error) {
+    logger.error("Error fetching user:", error);
+    throw error;
+  } finally {
+    await client.end();
+  }
+}
+
 async function getAllTodos(userId: number) {
+  const client = new Client(dbConfig);
   try {
     await client.connect();
 
     const query: string = `
-      SELECT *
+      SELECT id, public_id, name, description, created_at, ready
       FROM todos
       WHERE user_id = $1
     `;
@@ -47,16 +79,70 @@ async function getAllTodos(userId: number) {
 
     const todos = result.rows;
 
-    console.log(`Fetched ${todos.length} todos:`);
-    todos.forEach((todo) => console.log(todo));
-
     return todos; // Return the array of retrieved todo objects
   } catch (error) {
-    console.error("Error fetching todos:", error);
+    logger.error("Error fetching todos:", error);
     throw error; // Re-throw the error for further handling
   } finally {
     await client.end();
   }
 }
 
-export { addTodo, getAllTodos };
+async function updateTodo(
+  todoId: number,
+  updateData: Omit<TodoDto, "id" | "created_at" | "user_id">
+) {
+  const client = new Client(dbConfig);
+
+  try {
+    await client.connect();
+
+    const updateSet = Object.keys(updateData)
+      .map((key) => `${key} = $${Object.keys(updateData).indexOf(key) + 1}`)
+      .join(", ");
+
+    const query = `
+      UPDATE todos
+      SET ${updateSet}
+      WHERE public_id = $${Object.keys(updateData).length + 1}
+    `;
+
+    const values = [...Object.values(updateData), todoId]; // Combine update data and ID
+
+    const result = await client.query(query, values);
+
+    // Assuming successful update returns 1 row affected
+    if (result.rowCount === 1) {
+      logger.info("Todo updated successfully!");
+    }
+  } catch (error) {
+    logger.error("Error updating todo:", error);
+    throw error; // Re-throw the error for further handling
+  } finally {
+    await client.end();
+  }
+}
+
+async function deleteTodo(todoId: number): Promise<void> {
+  const client = new Client(dbConfig);
+
+  try {
+    await client.connect();
+
+    const query = `
+      DELETE FROM todos
+      WHERE public_id = $1
+    `;
+
+    await client.query(query, [todoId]);
+
+    console.log(`Todo with ID ${todoId} deleted successfully.`); // Optional logging
+  } catch (error) {
+    logger.error("Error deleting todo:", error);
+    throw error; // Re-throw the error for further handling
+  } finally {
+    await client.end();
+  }
+}
+
+export { addTodo, getAllTodos, getTodo, updateTodo, deleteTodo };
--- server/src/services/repository/user/user.ts
+++ server/src/services/repository/user/user.ts
@@ -1,31 +1,64 @@
-import { client } from "../../database";
+import { Client } from "pg";
+import { dbConfig } from "../../database";
 import { UserDto } from "./dto/user.dto";
+import logger from "../../logger";
 
-async function addUser(user: UserDto) {
-  const { name, email, password } = user; // Destructure user object
+async function getUserByEmail(email: string): Promise<UserDto | null> {
+  const client = new Client(dbConfig);
+  try {
+    await client.connect();
 
+    // Define the query to select user by email
+    const query = `
+      SELECT *
+      FROM users
+      WHERE email = $1
+    `;
+
+    // Execute the query with the provided email
+    const result = await client.query(query, [email]);
+
+    // Check if any user was found
+    if (result.rows.length === 0) {
+      return null; // Return null if no user found
+    }
+
+    // Return the first user from the results (assuming unique emails)
+    return result.rows[0];
+  } catch (error) {
+    logger.error("Error fetching user:", error);
+    throw error; // Re-throw the error for handling in the calling code
+  } finally {
+    // Ensure client connection is closed even if errors occur
+    await client.end();
+  }
+}
+
+async function createUser(user: Omit<UserDto, "id">) {
+  const { name, email, hash, public_id } = user; // Destructure user object
+  const client = new Client(dbConfig);
   try {
     await client.connect();
 
     // Assuming you have already executed the migration script to create the 'users' table
     const query = `
-        INSERT INTO users (name, email, password)
-        VALUES ($1, $2, $3)
+        INSERT INTO users (name, email, hash, public_id)
+        VALUES ($1, $2, $3, $4)
         RETURNING *; -- Return the inserted user record
       `;
 
-    const result = await client.query(query, [name, email, password]);
+    const result = await client.query(query, [name, email, hash, public_id]);
     const newUser = result.rows[0];
 
-    console.log(`User added successfully:`, newUser);
+    logger.info(`User added successfully:`, newUser);
 
     return newUser; // Return the inserted user object
   } catch (error) {
-    console.error("Error adding user:", error);
+    logger.error("Error adding user:", error);
     throw error; // Re-throw the error for further handling
   } finally {
     await client.end();
   }
 }
 
-export { addUser };
+export { createUser, getUserByEmail };
