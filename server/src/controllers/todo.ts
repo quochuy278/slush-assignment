993f280d3216d4900c448cbde5110a6c268f77b1
quochuy278 <quochuy270800@gmail.com>
2024-04-28 21:18:06 +0300

    server: create routes  for todo

--- server/src/controllers/todo.ts
+++ server/src/controllers/todo.ts
@@ -1,13 +1,104 @@
 import { Request, Response } from "express";
-import { getAllTodos } from "../services/repository/todo/todo";
+import {
+  addTodo,
+  getAllTodos,
+  deleteTodo as deleteTodoFromdb,
+  updateTodo as updateTodoFromDb,
+} from "../services/repository/todo/todo";
+import { v4 as uuidv4 } from "uuid";
+import { TodoDto } from "../services/repository/todo/dto/todo.dto";
+import { read } from "fs";
 
-const getTodos = async (request: Request, response: Response) => {
-  const userId = 1;
+const getTodos = async (request: any, response: Response) => {
+  const { userId } = request.user;
+
   try {
     const todos = await getAllTodos(userId);
-  } catch (error) {}
 
-  response.send("testing");
+    const formattedTodo = todos.map((todo: TodoDto) => {
+      const { public_id, ...rest } = todo;
+
+      return {
+        ...rest,
+        id: public_id,
+      };
+    });
+
+    return response.status(200).json(formattedTodo);
+  } catch (error) {
+    response.status(400).json(error);
+  }
 };
 
-export { getTodos };
+const createTodo = async (request: any, response: Response) => {
+  const { userId } = request.user;
+  const { name, description, ready } = request.body;
+
+  const createTodoData = {
+    name,
+    description,
+    ready,
+    public_id: uuidv4(),
+  };
+
+  try {
+    const todo = await addTodo(createTodoData, userId);
+
+    const { user_id, id, public_id, ...todoData } = todo;
+
+    return response.status(200).json({
+      id: public_id,
+      ...todoData,
+    });
+  } catch (error) {
+    response.status(400).json(error);
+  }
+};
+
+const updateTodo = async (request: any, response: Response) => {
+  const { name, description, ready } = request.body;
+
+  const { todoId } = request.params;
+  console.log(
+    "🚀 ~ updateTodo ~ name, description, ready:",
+    name,
+    description,
+    ready
+  );
+
+  const updateData = {
+    name: name as string,
+    description: description as string,
+    ready: ready as boolean,
+    public_id: todoId as string,
+  };
+
+  try {
+    const todo = await updateTodoFromDb(todoId, updateData);
+
+    response.status(200).json({
+      message: "Update todo successfully",
+    });
+  } catch (error) {
+    response.status(400).json(error);
+  }
+};
+
+const deleteTodo = async (request: any, response: Response) => {
+  const { userId } = request.user;
+
+  const { todoId } = request.params;
+  console.log("🚀 ~ deleteTodo ~ todoId:", todoId);
+
+  try {
+    await deleteTodoFromdb(todoId);
+
+    return response.status(200).json({
+      message: "Successfully delete todo",
+    });
+  } catch (error) {
+    response.status(400).json(error);
+  }
+};
+
+export { getTodos, createTodo, updateTodo, deleteTodo };
--- server/src/routes/todo.ts
+++ server/src/routes/todo.ts
@@ -1,8 +1,21 @@
 import { Router } from "express";
-import { getTodos } from "../controllers/todo";
+import {
+  createTodo,
+  deleteTodo,
+  getTodos,
+  updateTodo,
+} from "../controllers/todo";
+import verifyJwtToken from "../middleware/authenticate";
+import { validateTodoBody } from "../middleware/validation";
 
 const router = Router();
 
-router.get("/todos", getTodos);
+router.get("/todos", verifyJwtToken, getTodos);
+
+router.post("/todo", verifyJwtToken, validateTodoBody, createTodo);
+
+router.patch("/todo/:todoId", verifyJwtToken, validateTodoBody, updateTodo);
+
+router.delete("/todo/:todoId", verifyJwtToken, deleteTodo);
 
 export const todoRoutes: Router = router;
