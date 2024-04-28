3ee7af33595bde03c9c5cc8991664754eda36020
quochuy278 <quochuy270800@gmail.com>
2024-04-28 21:14:21 +0300

    client: add homepage

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Todo from "../features/Todo/Todo";
+
+const HomePage = () => {
+  return (
+    <div className="flex flex-col min-h-[86vh] w-screen">
+      {" "}
+      {/* Set min-height for viewport */}
+      <div className="flex-grow bg-purple-light p-4 justify-center flex">
+        {" "}
+        {/* TodoList container */}
+        <Todo />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
